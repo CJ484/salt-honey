ef4068ada70e9f5d628a3f91021ee616d878c7ed
Carlos Coreas <ccoreas484@gmail.com>
2026-07-10 00:37:48 -0400

    Add SOCIAL_LINKS constant with Instagram and TikTok details, including icons and URLs

---
+++ lib/const.tsx
@@ -0,0 +1,13 @@
+import { LuInstagram } from 'react-icons/lu';
+import { FaTiktok} from 'react-icons/fa';
+
+export const SOCIAL_LINKS = {
+    instagram: {
+        icon: LuInstagram,
+        url: 'https://www.instagram.com/saltandhoneycaffe/',
+    },
+    tiktok: {
+        icon: FaTiktok,
+        url: 'https://www.tiktok.com/@saltandhoneycaffe',
+    },
+}
