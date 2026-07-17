d98f1d4202cef65e2679dc05bd3eda4ff363847e
Carlos Coreas <ccoreas484@gmail.com>
2026-07-17 15:54:45 -0400

    Add ShinyText component for animated text effects and update index.ts to export it alongside Footer component

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,5 @@
 export * from "./ui/button";
 export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
 export { default as FadeUp } from "./FadeUp";
+export { default as ShinyText } from "./ShinyText";
